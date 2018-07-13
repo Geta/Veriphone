bf6b21a0ebd29e81bd5d11fa66d85dcb8d4b0d11
Martins Savickis <morowinder@gmail.com>
2018-07-13 17:03:30 +0300

    Update DecimalExtensions.cs
    https://dotnetfiddle.net/xmCu43

--- src/Geta.Commerce.Payments.Verifone.HostedPages/Extensions/DecimalExtensions.cs
+++ src/Geta.Commerce.Payments.Verifone.HostedPages/Extensions/DecimalExtensions.cs
@@ -7,7 +7,7 @@ namespace Geta.Commerce.Payments.Verifone.HostedPages.Extensions
     {
         public static string ToVerifoneAmountString(this decimal number)
         {
-            return Math.Round(number, 2).ToString(CultureInfo.InvariantCulture).Replace(".", "");
+            return ((int)(Math.Round(number, 2)*100)).ToString();
         }
     }
 }
