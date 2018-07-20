24a3e5f919cb8a319544e99bfde894415056ab1a
Martins Savickis <martins@geta.no>
2018-07-20 14:35:40 +0300

    add unit tests

---
+++ src/Geta.Verifone.Tests/Extensions/TruncateTest.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using Geta.Verifone.Extensions;
+using Xunit;
+
+namespace Geta.Verifone.Tests.Extensions
+{
+    public class TruncateTest
+    {
+        [Fact]
+        public void StringLongerThanLength()
+        {
+            var value = "12345678";
+            var length = 3;
+            var expected = "123";
+
+            var actual = value.Truncate(length);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void StringShorterThanLength()
+        {
+            var value = "123";
+            var length = 99;
+            var expected = "123";
+
+            var actual = value.Truncate(length);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void StringSameAsLength()
+        {
+            var value = "123";
+            var length = 3;
+            var expected = "123";
+
+            var actual = value.Truncate(length);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void StringReferenceDoesNotChange()
+        {
+            var value = "123456";
+            var length = 3;
+            var expected = "123456";
+
+            var actual = value.Truncate(length);
+
+            value.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/Geta.Verifone.Tests/Models/VerifonePaymentRequestParameterLengthTest.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.InteropServices;
+using FluentAssertions;
+using Geta.Commerce.Payments.Verifone.HostedPages.Models;
+using Xunit;
+
+namespace Geta.Verifone.Tests.Models
+{
+    public class VerifonePaymentRequestParameterLengthTest
+    {
+        [Fact]
+        public void StringTooLong()
+        {
+            var request = new VerifonePaymentRequest();
+            var value = "1234567890123456789012345678901234567890";
+            var expected = "123456789012345678901234567890";
+
+            request.BuyerFirstName = value;
+
+            request.BuyerFirstName.Should().BeEquivalentTo(expected);
+            request.BuyerFirstName.Length.Should().Be(VerifoneConstants.ParameterMaxLength.BuyerFirstName);
+        }
+        
+        [Fact]
+        public void StringTooLong_ByOneCharacte()
+        {
+            var request = new VerifonePaymentRequest();
+            var value = "1234567890123456789012345678901";
+            var expected = "123456789012345678901234567890";
+
+            request.BuyerFirstName = value;
+
+            request.BuyerFirstName.Should().BeEquivalentTo(expected);
+            request.BuyerFirstName.Length.Should().Be(VerifoneConstants.ParameterMaxLength.BuyerFirstName);
+        }
+        
+        [Fact]
+        public void StringShorter()
+        {
+            var request = new VerifonePaymentRequest();
+            var value = "123";
+
+            request.BuyerFirstName = value;
+
+            request.BuyerFirstName.Should().BeEquivalentTo(value);
+            request.BuyerFirstName.Length.Should().Be(value.Length);
+        }
+        
+        [Fact]
+        public void StringShorter_ByOneCharacter()
+        {
+            var request = new VerifonePaymentRequest();
+            var value = "1234567890123456789";
+
+            request.BuyerFirstName = value;
+
+            request.BuyerFirstName.Should().BeEquivalentTo(value);
+            request.BuyerFirstName.Length.Should().Be(value.Length);
+        }
+        
+        [Fact]
+        public void StringExactlyMaxLength()
+        {
+            var request = new VerifonePaymentRequest();
+            var value = "123456789012345678901234567890";
+
+            request.BuyerFirstName = value;
+
+            request.BuyerFirstName.Should().BeEquivalentTo(value);
+            request.BuyerFirstName.Length.Should().Be(value.Length);
+        }
+    }
+}
