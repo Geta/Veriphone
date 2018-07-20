5157528b340cb044fb939a31ca5a24803d2a9386
Martins Savickis <morowinder@gmail.com>
2018-07-20 14:54:24 +0300

    Ensure maximum parameter length, add tests (#8)
    Truncate parameter length according to API specification

---
+++ src/Geta.Verifone.Tests/Extensions/ToVerifoneAmountStringTest.cs
@@ -0,0 +1,86 @@
+using FluentAssertions;
+using Geta.Commerce.Payments.Verifone.HostedPages.Extensions;
+using Xunit;
+
+namespace Geta.Verifone.Tests.Extensions
+{
+    public class ToVerifoneAmountStringTest
+    {
+        [Fact]
+        public void WholeNumber()
+        {
+            var value = 65m;
+            var expected = "6500";
+
+            var actual = value.ToVerifoneAmountString();
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void SingleDecimalPlace()
+        {
+            var value = 65.0m;
+            var expected = "6500";
+
+            var actual = value.ToVerifoneAmountString();
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void TwoDecimalPlaces()
+        {
+            var value = 65.00m;
+            var expected = "6500";
+
+            var actual = value.ToVerifoneAmountString();
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void MultipleDecimalPlaces()
+        {
+            var value = 65.000000m;
+            var expected = "6500";
+
+            var actual = value.ToVerifoneAmountString();
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void Rounding()
+        {
+            var value = 65.009m;
+            var expected = "6501";
+
+            var actual = value.ToVerifoneAmountString();
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void RoundingMidpoint()
+        {
+            var value = 65.55555m;
+            var expected = "6556";
+
+            var actual = value.ToVerifoneAmountString();
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void Roundingbankers()
+        {
+            var value = 65.045m;
+            var expected = "6504";
+
+            var actual = value.ToVerifoneAmountString();
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/Geta.Verifone/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Geta.Verifone.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (!string.IsNullOrEmpty(value) && value.Length > maxLength)
+            {
+                return value.Substring(0, maxLength);
+            }
+
+            return value;
+        }
+    }
+}
