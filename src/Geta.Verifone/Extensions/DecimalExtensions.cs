74e6ba1cbdca116f4b9b33ba733e0c4d3721aa1f
Martins Savickis <martins@geta.no>
2018-07-25 19:29:43 +0300

    comment explaining ToVerifoneAmountString

--- src/Geta.Verifone/Extensions/DecimalExtensions.cs
+++ src/Geta.Verifone/Extensions/DecimalExtensions.cs
@@ -4,8 +4,15 @@ namespace Geta.Verifone.Extensions
 {
     public static class DecimalExtensions
     {
+        /// <summary>
+        /// Converts decimal amount to string representation without decimal separator
+        /// </summary>
+        /// <param name="amount">Decimal amount</param>
+        /// <returns>Verifone amount string</returns>
+        /// <example>645.00 returns 64500</example>
         public static string ToVerifoneAmountString(this decimal amount)
         {
+            // multiply to move decimal point 2 places and round to whole number
             return ((int)(Math.Round(amount * 100, 0))).ToString();
         }
     }
